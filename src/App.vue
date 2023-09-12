a40df27ba62c457feade3f889fb9123be5ddfa82
jikantoki <61790160+jikantoki@users.noreply.github.com>
2023-09-12 22:00:05 +0900

    make navigation

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template lang="pug">
 v-app
   v-main
+    .links
+      .linkTop
+        router-link(to="/")
+          .navButton Top
+      .linkOsakana
+        router-link(to="/a")
+          .navButton Osakana
     router-view
-    ul
-      ll
-        router-link(to="/") Top
-      ll
-        router-link(to="/a") Osakana
 </template>
 
 <script>
@@ -28,4 +30,29 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+:root {
+  transition: all 0.2s;
+}
+.links {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  div > a > div {
+    width: 8em;
+    text-align: center;
+    font-size: 1.5em;
+    color: black;
+    margin: 0.1em;
+    padding: 0.1em 0.3em;
+    border-radius: 9999em;
+    &:hover {
+      background-color: #333333;
+      color: #e6e6e6;
+    }
+  }
+}
+a {
+  text-decoration: none;
+}
+</style>
