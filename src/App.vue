e0dd694e933f5d2c63a6e53576b602490d748355
jikantoki <61790160+jikantoki@users.noreply.github.com>
2023-09-13 04:17:10 +0900

    Learn Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template lang="pug">
-v-app
+v-app(ontouchstart="")
+  header
+    v-app-bar
+      template(v-slot:append)
+        v-btn(icon="mdi-magnify")
+        v-btn(icon="mdi-dots-vertical")
+      v-app-bar-nav-icon(@click="toggleDrawer()")
+      v-toolbar-title {{ PackageJson.name }}
+    v-navigation-drawer(v-model="drawer" fixed temporary)
+      v-list(nav dense lines="zero")
+        v-list-item-group
+          v-list-item(v-for="navigationItem in NavigationList")
+            li.nav(@click="a(navigationItem.url)")
+              p {{ navigationItem.name }}
   v-main
     .links
       .linkTop
@@ -9,50 +22,100 @@ v-app
         router-link(to="/a")
           .navButton Osakana
     router-view
+  v-footer
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PackageJson from '../package.json'
+import Functions from '../functions/Functions'
+import NavigationList from './items/itemNavigationList'
 
 export default {
   name: 'App',
-
   components: {
-    HelloWorld
+    PackageJson,
+    NavigationList
   },
-
-  data: () => ({
-    //
-  }),
-  mounted: () => {
-    console.log('hello')
+  data() {
+    return {
+      PackageJson: PackageJson,
+      NavigationList: NavigationList,
+      drawer: false
+    }
+  },
+  mounted() {
+    PackageJson.name = Functions.ifEnglishStartUpper(PackageJson.name)
+  },
+  methods: {
+    toggleDrawer() {
+      if (this.drawer === false) {
+        this.drawer = true
+      } else {
+        this.drawer = false
+      }
+    },
+    /**
+     * <p>aタグと同じ動きをするし、pjaxになる</p>
+     * <p>外部URLの場合、新しいタブで開く</p>
+     * @param {string} url 転送したいURL（ルートからのパス）
+     * @returns {int} 内部リンクなら0、外部ドメインなら1
+     */
+    a(url) {
+      if (url.slice(0, 4) === 'http') {
+        window.open(url, '_blank')
+        return 1
+      } else {
+        this.$router.push(url)
+        return 0
+      }
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-:root {
-  transition: all 0.2s;
+* {
+  user-select: none;
+  list-style: none;
+  transition: all 0.14s;
 }
-.links {
+li.nav {
+  width: 100%;
+  height: 3em;
+  cursor: pointer;
   display: flex;
-  justify-content: space-evenly;
   align-items: center;
-  div > a > div {
-    width: 8em;
-    text-align: center;
-    font-size: 1.5em;
-    color: black;
-    margin: 0.1em;
-    padding: 0.1em 0.3em;
-    border-radius: 9999em;
-    &:hover {
-      background-color: #333333;
-      color: #e6e6e6;
-    }
+  &:hover::after {
+    content: '>';
+    margin-left: 1em;
+    font-weight: bold;
   }
 }
-a {
-  text-decoration: none;
+#app {
+  .links {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    box-sizing: border-box;
+    div > a {
+      color: black;
+      margin: 0.1em;
+      padding: 0.1em 0.3em;
+      border-radius: 9999em;
+      display: block;
+      div {
+        width: 8em;
+        font-size: 1.5em;
+        text-align: center;
+      }
+      &:hover {
+        background-color: #333333;
+        color: #e6e6e6;
+      }
+    }
+    a {
+      text-decoration: none;
+    }
+  }
 }
 </style>
